7061947e907f592cfa17b25c496c458c936dd038
victorRangelX <77605163+victorRangelX@users.noreply.github.com>
2026-03-27 00:10:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-app.use(cors());
 
 const app = express();
-app.use(express.json());
-app.use(express.static("."));
 
-const API_KEY = "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";
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.post("/preguntar", async (req, res) => {
   try {
@@ -15,27 +15,25 @@ app.post("/preguntar", async (req, res) => {
     const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${API_KEY}`,
+        "Authorization": `Bearer ${process.env.API_KEY}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
         model: "openai/gpt-3.5-turbo",
         messages: [
-  {
-    role: "system",
-    content: "Eres un chef experto. Responde siempre con recetas claras, con ingredientes y pasos, debes priorisar dar recetas que sean claras con pasos claros de seguir y con el objetivo de enseñar a cocinar, podrias dar lar recetas en 3 pasos, ingredientes, instruciones de elvaoracion , y explicar el nivel de difucltad, asi como tips de comoo prerararlo,"
-  },
-  {
-    role: "user",
-    content: mensaje
-  }
-]
+          {
+            role: "system",
+            content: "Eres un chef experto. Responde con recetas claras, ingredientes y pasos."
+          },
+          {
+            role: "user",
+            content: mensaje
+          }
+        ]
       })
     });
 
     const data = await response.json();
-
-    console.log(data);
 
     const text = data?.choices?.[0]?.message?.content || "Sin respuesta";
 
@@ -47,7 +45,6 @@ app.post("/preguntar", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("http://localhost:3000");
-}); 
-
+app.listen(PORT, () => {
+  console.log("Servidor corriendo");
+});
